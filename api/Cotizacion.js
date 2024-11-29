ad0d2a4afeaf39f671281d659509bf094320ff74
Mishel450 <tomasmishel18@gmail.com>
2024-11-29 13:33:13 -0800

    testeos de post

--- api/Cotizacion.js
+++ api/Cotizacion.js
@@ -1,10 +1,49 @@
 import { app, supabase } from "../index.js";
 
 const { data, error } = await supabase
-  .from('Importacion')
+  .from('Trabajo')
   .select()
-  .match()
 
-app.get("/hola", (req, res) => {
-    res.send(data);
+async function createCommon() {
+  try {
+  const { data, error } = await supabase
+    .from('Common')
+    .insert({})
+    .select("id")
+  return (Object.values(data[0]))
+  } catch {
+    return ("exploto we")
+  }
+}
+
+async function createTrabajo(id, body) {
+  try {
+  const { data, error } = await supabase
+    .from('Trabajo')
+    .insert({ "id": id, ...body })
+    .select()
+  return (id)
+  } catch {
+    return ("explote yo amigo mishel")
+  }
+}
+
+app.get("/trabajos", (req, res) => {
+  res.send(data);
 });
+
+app.post("/trabajos", async (req, res) => {
+  const body = req.body
+  try {
+    const id = await createCommon()
+    let trueid = 1
+    for (let x of id) {
+      trueid = x
+    }
+    ///const data = await createTrabajo(trueid, body)
+    res.send(trueid)
+  } catch {
+    res.send("no we")
+  }
+}
+)
