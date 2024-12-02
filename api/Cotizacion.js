cd0a5fd2b5225eabb5c6c5b701970fa7177e9cc2
Mishel450 <tomasmishel18@gmail.com>
2024-12-02 13:14:56 -0800

    cambios en get, post y post de importacion agregado

--- api/Cotizacion.js
+++ api/Cotizacion.js
@@ -1,38 +1,15 @@
 import { app, supabase } from "../index.js";
 
-async function getTrabajo() {
+async function getData(tableName) {
   const { data, error } = await supabase
-    .from('Trabajo')
-    .select()
-  return (data)
-}
-
-async function getVariables() {
-  const { data, error } = await supabase
-    .from('Variable')
-    .select()
-  return (data)
-}
-
-async function getDeposito() {
-  const { data, error } = await supabase
-    .from('Deposito')
-    .select()
-  return (data)
-}
-
-async function getServicios() {
-  const { data, error } = await supabase
-    .from('Servicio')
-    .select()
-  return (data)
-}
-
-async function getLocalidad() {
-  const { data, error } = await supabase
-    .from('Localizacion')
-    .select()
-  return (data)
+    .from(tableName)
+    .select();
+  
+  if (error) {
+    console.error("Error fetching data from ${tableName}: ", error);
+    return null; // O manejar el error como prefieras
+  }
+  return data;
 }
 
 async function createCommon() {
@@ -51,7 +28,15 @@ async function createTrabajo(id, body) {
   try {
     const { data, error } = await supabase
       .from('Trabajo')
-      .insert({ id, ...body })
+      .insert({
+        id, "origen": body["origen"],
+        "destino": body["destino"],
+        "validez_ini": body["validez_ini"],
+        "validez_fin": body["validez_fin"],
+        "cliente": body["cliente"], "incoterm": body["incoterm"],
+        "medio": body["medio"], "costo": body["costo"],
+        "Servicio": body["Servicio"], "tt": body["tt"]
+      })
       .select()
     return (data)
   } catch {
@@ -59,50 +44,69 @@ async function createTrabajo(id, body) {
   }
 }
 
+async function createImportacion(id, body) {
+  try {
+    const { data, error } = await supabase
+      .from('Importacion')
+      .insert({
+        id, "exclusivo": body["exclusivo"], "consolidado": body["consolidado"],
+        "gastos_origen": body["gastos_origen"], "tarifa": body["tarifa"],
+        "serv_admin": body["serv_admin"], "handling": body["handling"],
+        "deposito": body["deposito"], "unif_factura": body["unif_factura"],
+        "tlx": body["tlx"], "seguro": body["seguro"],
+        "deposito_local": body["deposito_local"], "salida_depo": body["salida_depo"]
+      })
+      .select()
+    return (data)
+  } catch {
+    return("importacion exploto!!")
+  }
+}
+
 async function updateTrabajo(body) {
   let bodyWithoutId = {}
   let id = null
-  try { 
-  for (let i in body) {
-    if (i == "id") {
-      id = body[i]
-    } else {
-      bodyWithoutId[i] = body[i]
+  try {
+    for (let i in body) {
+      if (i == "id") {
+        id = body[i]
+      } else {
+        bodyWithoutId[i] = body[i]
+      }
     }
-  }
-  const { data, error } = await supabase
-    .from('Trabajo')
-    .update({...bodyWithoutId})
-    .eq('id', id)
-    .select()
-  return (data)
+    const { data, error } = await supabase
+      .from('Trabajo')
+      .update({ ...bodyWithoutId })
+      .eq('id', id)
+      .select()
+    return (data)
   } catch {
     return ("hola soy mister error jejej")
   }
 }
 
 app.get("/trabajos", async (req, res) => {
-  const data = await getTrabajo();
+  const data = await getData('Trabajo');
   res.json(data);
 });
 
 app.get("/variables", async (req, res) => {
-  const data = await getVariables();
+  const data = await getData('Variable');
   res.json(data);
 });
 
 app.get("/deposito", async (req, res) => {
-  const data = await getDeposito();
+  const data = await getData('Deposito');
   res.json(data);
 });
 
 app.get("/servicios", async (req, res) => {
-  const data = await getServicios();
+  const data = await getData('Servicio');
   res.json(data);
 });
 
 app.get("/localidad", async (req, res) => {
-  const data = await getLocalidad();
+  const data = await getData('Localizacion');
   res.send(data);
 });
 
@@ -124,5 +128,18 @@ app.put("/trabajos", async (req, res) => {
     res.json(data)
   } catch {
     res.send("hola soy error")
+  }
+})
+
+app.post("/trabajos/importacion", async (req, res) => {
+  const body = req.body
+  try {
+    const id = await createCommon()
+    const datat = await createTrabajo(id, body)
+    const datai = await createImportacion(id, body)
+    const data = {"trabajo" :datat, "importacion" :datai}
+    res.json(data)
+  } catch {
+    res.send("no we")
   }
 })
