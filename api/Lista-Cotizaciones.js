9c833da768f14706447169adc552dcc8b6165e77
Korchea <guilllermo.vega@gmail.com>
2024-12-05 10:27:32 -0800

    Mod exportacion

--- api/Lista-Cotizaciones.js
+++ api/Lista-Cotizaciones.js
@@ -25,6 +25,62 @@ async function getData(tableName) {
     return data?.[0] || null; // Retorna el primer elemento o null si no hay datos
   }  
 
+
+  async function createTrabajo(id, body) {
+    try {
+      const { data, error } = await supabase
+        .from('Trabajo')
+        .insert({
+          id, "origen": body["origen"],
+          "destino": body["destino"],
+          "validez_ini": body["validez_ini"],
+          "validez_fin": body["validez_fin"],
+          "cliente": body["cliente"], "incoterm": body["incoterm"],
+          "medio": body["medio"], "costo": body["costo"],
+          "Servicio": body["Servicio"], "tt": body["tt"]
+        })
+        .select()
+      return (data)
+    } catch {
+      return ("exploto we")
+    }
+  }
+  
+  async function createImportacion(id, body) {
+    try {
+      const { data, error } = await supabase
+        .from('Importacion')
+        .insert({
+          id, "exclusivo": body["exclusivo"], "consolidado": body["consolidado"],
+          "gastos_origen": body["gastos_origen"], "tarifa": body["tarifa"],
+          "serv_admin": body["serv_admin"], "handling": body["handling"],
+          "deposito": body["deposito"], "unif_factura": body["unif_factura"],
+          "tlx": body["tlx"], "seguro": body["seguro"],
+          "deposito_local": body["deposito_local"], "salida_depo": body["salida_depo"]
+        })
+        .select()
+      return (data)
+    } catch {
+      return("importacion exploto!!")
+    }
+  }
+
+  async function createExportacion(id, body) {
+    try {
+      const { data, error } = await supabase
+        .from('Exportacion')
+        .insert({
+          id, "goec": body["goec"], "trasint": body["trasint"],
+          "gitp": body["gitp"], "gam": body["gam"],
+          "cc": body["cc"], "flete": body["flete"],
+        })
+        .select()
+      return (data)
+    } catch {
+      return ("Exportacion exploto!!")
+    }
+  }
+  
   // GET api/trabajos/: Trae todas las cotizaciones
   app.get("/trabajos", async (req, res) => {
     const data = await getData('Trabajo');
@@ -43,3 +99,21 @@ app.get("/trabajos/:id", async (req, res) => {
     res.json(data);
   });
   
+  // POST api/trabajos/:id: Duplica una cotizacion
+  app.post("/trabajos/:id", async (req, res) => {
+    const body = req.body
+  try {
+    const id = await createCommon()
+    const datat = await createTrabajo(id, body)
+    if (body["importacion"] == true) {
+        const datai = await createImportacion(id, body)
+        const data = {"trabajo" :datat, "importacion" :datai}
+    } else {
+        const datae = await createExportacion(id, body)
+        const data = {"trabajo" :datat, "exportacion" :datae}
+    }
+    res.json(data)
+  } catch {
+    res.send("no we")
+  }
+  })
