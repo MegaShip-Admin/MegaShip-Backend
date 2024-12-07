d6790816879bba0fbbc80ef183431975acbc0f05
Mishel450 <tomasmishel18@gmail.com>
2024-12-07 12:13:20 -0800

    arreglando el codigo de guille

--- api/Login.js
+++ api/Login.js
@@ -1,29 +1,29 @@
 import { app, supabase } from "../index.js";
 
 async function getDataByEmail(tableName, email) {
-    const { data, error } = await supabase
-      .from(tableName)
-      .select()
-      .eq('email', email); // Filtra por la columna `id`
-  
-    if (error) {
-      console.error(`Error fetching data from ${tableName} with Email ${email}:`, error);
-      return null; // Manejar el error como prefieras
-    }
-    return data?.[0] || null; // Retorna el primer elemento o null si no hay datos
-  } 
+  const { data, error } = await supabase
+    .from(tableName)
+    .select()
+    .eq('email', email) // Filtra por la columna `id`
+  if (error) {
+    console.error(`Error fetching data from ${tableName} with Email ${email}:`, error);
+    return null; // Manejar el error como prefieras
+  }
+  return data?.[0] || null; // Retorna el primer elemento o null si no hay datos
+}
 
-    // GET api/vendedores/:email: Trae una cotización por Email
-app.post("/vendedor/", async (req, res) => {
-    const body = req.body
-    const data = await getDataById('Vendedor', body["email"]);
-  
-    if (!data) {
-      return res.status(404).json({ error: "Vendedor no encontrado" }); // Manejo de error si no se encuentra
-    }
-    if (data["password"] != body["password"]) {
-        return res.status(14).json({ error: "La contraseña no es la correcta" })
-    }
-  
+// GET api/vendedores/:email: Trae una cotización por Email
+app.post("/vendedor", async (req, res) => {
+  const body = req.body
+  const data = await getDataByEmail('Vendedor', body["email"]);
+
+  if (!data) {
+    return res.status(404).json({ error: "Vendedor no encontrado" }); // Manejo de error si no se encuentra
+  }
+  if (data["password"] != body["password"]) {
+    res.status(401).json({ error: "La contraseña no es la correcta" })
+  }
+  else {
     res.json(data);
-  });
+  }
+});
